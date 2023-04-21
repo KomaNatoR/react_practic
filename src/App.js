7c7fd5f7fad3d061975284162e391c8c4a14f8f6
Serhii Markov <goo.komalel@gmail.com>
2023-04-21 16:42:06 +0300

    started

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <h2 className="header_title">
+      HI!
+    </h2>
+  );
+}
+
+export default App;
---
+++ src/components/PostList.jsx
@@ -0,0 +1,17 @@
+
+
+const PostList = () => {
+    return (
+        <>
+            <h2 class="post-list-title">Post List</h2>
+
+            <ul class="post-list">
+                <li class="post-list-item">
+                    <img class="post-list-img" src="" alt="" />
+                    <h4 class="post-list-item-title">Post title</h4>
+                    <p class="post-list-item-text">Post text</p>
+                </li>
+            </ul>
+        </>
+    )
+};
