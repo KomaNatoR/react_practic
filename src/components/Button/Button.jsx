6f7214667c2bcf4324648a3da65991a32c7ddc99
Serhii Markov <goo.komalel@gmail.com>
2023-05-09 22:52:31 +0300

    start final project!

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,12 @@
+import { Btn } from "./button.styled";
+
+const Button = () => {
+    return (
+        <Btn>
+            Hello!
+        </Btn>
+    )
+};
+
+
+export default Button
---
+++ src/components/Button/button.styled.js
@@ -0,0 +1,6 @@
+import styled from "@emotion/styled";
+
+export const Btn = styled.button`
+    background-color: ${({ theme }) => theme.colors.lightBlue};
+    box-shadow: ${({ theme }) => theme.shadows.small};
+`;
